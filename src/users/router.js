f44d45b645a0de13c4a1a339b2947f457efcfd5a
Jason00blabal <cuhaoyan@gmail.com>
2019-11-03 23:09:56 +0800

    實作啟用帳號流程

--- src/users/router.js
+++ src/users/router.js
@@ -6,5 +6,6 @@ const usersController = require('./usersController');
 router.get('/isLogin', (req, res) => usersController.isLogin(req, res));
 router.post('/signup', (req, res) => usersController.signup(req, res));
 router.post('/login', (req, res) => usersController.login(req, res));
+router.put('/active', (req, res) => usersController.activeUser(req, res));
 
 module.exports = router;
