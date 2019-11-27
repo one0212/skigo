c732d4c72efb8e4ea4b7495895f124abf01876d4
Allen <asd9fgh9@gmail.com>
2019-11-27 23:43:26 +0800

    新增篩選排序

--- src/areas/router.js
+++ src/areas/router.js
@@ -23,23 +23,23 @@ bluebird.promisifyAll(db);
 
 router.post('/ticket-list', urlencodedParser, (req, res) => {
   // console.log(req.body);
-  const Sort = '';
-  // if (req.body.sort == true) {
-  //   Sort = 'ORDER BY `ticket_price` DESC ';
-  //   console.log(`從高到低${Sort}`);
-  // } else if (req.body.sort == false) {
-  //   Sort = 'ORDER BY `ticket_price` ASC ';
-  //   console.log(`從低到高${Sort}`);
-  // } else {
-  //   console.log('no sort!');
-  //   Sort = '';
-  // }
+  let Sort = '';
+  if (req.body.sort === true) {
+    Sort = 'ORDER BY `ticket_price` DESC ';
+    // console.log(`從高到低${Sort}`);
+  } else if (req.body.sort == false) {
+    Sort = 'ORDER BY `ticket_price` ASC ';
+    // console.log(`從低到高${Sort}`);
+  } else {
+    // console.log('no sort!');
+    Sort = '';
+  }
 
   // output 過濾重複課程的陣列
   let output = [];
   const addrData = ['北海道', '青森縣', '山形縣', '新瀉縣'];
   const typeData = ['纜車券', '租借券', '超值套票'];
-  const ageData = ['一般', '優待', '敬老'];
+  const ageData = ['成人', '兒童', '敬老'];
   const dayData = ['一日', '二日', '三日'];
 
   const addrB = req.body.name.slice(0, 4);
@@ -79,7 +79,7 @@ router.post('/ticket-list', urlencodedParser, (req, res) => {
 
   let where = ' WHERE 1 ';
   if (addr.length) {
-    where += ` AND ticket_addr IN ('${addr.join("','")}') `;
+    where += ` AND area_addr IN ('${addr.join("','")}') `;
   }
 
   if (type.length) {
@@ -92,7 +92,7 @@ router.post('/ticket-list', urlencodedParser, (req, res) => {
   if (day.length) {
     where += ` AND ticket_use_day IN ('${day.join("','")}') `;
   }
-
+  console.log(where);
   // console.log(`SELECT * FROM ticket ${where} `);
   db.query(`SELECT * FROM ticket ${where} ${Sort}`, (error, results) => {
     // 印出變數代稱是否有成功撈到(為字串)
@@ -102,7 +102,7 @@ router.post('/ticket-list', urlencodedParser, (req, res) => {
     // 並用map重新return，存入output陣列
 
     // console.log(results);
-
+    console.log(results);
     output = results.map((el) => {
       if (output.indexOf(el.ticket_sid) === -1) {
         output.push(el.ticket_sid);
