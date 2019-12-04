b9bd85a2188993623704a8088937e75276294e8a
one0212 <51477847+one0212@users.noreply.github.com>
2019-12-04 13:10:02 +0800

    add - avatar api

--- src/users/UserSession.js
+++ src/users/UserSession.js
@@ -1,8 +1,9 @@
 export default class UserSession {
-  constructor(id, email, role, cart = {}) {
+  constructor(id, email, role, avatar, cart = {}) {
     this.id = id;
     this.email = email;
     this.role = role;
+    this.avatar = avatar;
     this.cart = cart;
   }
 }
